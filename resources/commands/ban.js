1b185bb965757b0b168998c2ec192d601d7373e9
Kayui Mineko <63090353+KyIMineko@users.noreply.github.com>
2020-09-08 19:50:50 +0700

    Update ban.js

--- resources/commands/ban.js
+++ resources/commands/ban.js
@@ -32,8 +32,8 @@ module.exports = {
   cooldown: 30000, // Command cooldown
   examples: ["ban @KyIDevs Essentials#0309 7", "ban 751780624704077906 0 Go away", "ban @KyIDevs Essentials#0309"], // Command Examples
   usage: ["<target>", "[days]", "[reason]"], // Command Usage
-  permissions: ["SEND_MESSAGES", "READ_MESSAGES", "BAN_MEMBERS"], // Command Permissions
-  memberPermissions: ["SEND_MESSAGES", "READ_MESSAGES", "BAN_MEMBERS"], // User is required to have these permissions
+  permissions: ["SEND_MESSAGES", "READ_MESSAGE_HISTORY", "BAN_MEMBERS"], // Command Permissions
+  memberPermissions: ["SEND_MESSAGES", "READ_MESSAGE_HISTORY", "BAN_MEMBERS"], // User is required to have these permissions
   admin: false, // Command is admin only
   async execute(client, command, message, args, auth, channel, guild) { // Function async execute()
     // Command Starts Here
