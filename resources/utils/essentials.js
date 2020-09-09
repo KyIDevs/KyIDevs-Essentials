67fb593f9f938908aa09938b4746069daa23bc87
Kayui Mineko <63090353+KyIMineko@users.noreply.github.com>
2020-09-09 17:05:55 +0700

    Update essentials.js

--- resources/utils/essentials.js
+++ resources/utils/essentials.js
@@ -86,7 +86,7 @@ module.exports = {
       return text;
     }
   },
-  placeHolder(client, string) {
+  async placeHolder(client, string) {
     const owner = client.users.cache.get(client.config.admin.owner.id);
     if (typeof string === "string") {
       return string
