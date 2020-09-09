3c15cb379cd527819f6248b2669647e9211fb0a2
Kayui Mineko <63090353+KyIMineko@users.noreply.github.com>
2020-09-09 17:25:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,33 +122,16 @@ mongoose.connect(
 
 // Event Emitted: Ready //
 client.on("ready", async () => {
-  client.footer = await Essentials.placeHolder(client, config.client.settings.footer);
-  let activity = {};
-  activity.name = await Essentials.placeHolder(client, config.client.presence.activity.default.name);
-  activity.type = config.client.presence.activity.default.name;
-  activity.status = config.client.presence.activity.status;
-  try {
-    // Set Activity every 30 seconds
-    setInterval(() => {
-      client.user.setActivity(
-        activity.name, {
-          type: activity.type
-        }
-      );
-      client.user.setStatus(activity.status);
-    }, 5000);
-  } catch (error) {
-    Essentials.log(client, error);
-  }
-  
-  // Bot Ready Log //
-  console.log(
-    `Logged in as ${client.user.tag}.\n`
-    + `There are ${client.users.cache.size} users and/or bots online.\n`
-    + `${client.user.tag} connected to:\n${client.guilds.cache
-      .map(g => g.name)
-      .join(", ")}`
-  );
+  const event = client.events.get("ready");
+  if (event) {
+    try {
+      await event.execute(
+        client, event
+      )
+    } catch (err) {
+      Essentials.log(client, err)
+    }
+  } 
 });
 
 // Guild Join Event //
